913d537417dfd5620f31f40f27b1d3adb91a2b89
xujianlei <xujianlei@163.com>
2023-01-03 11:36:55 +0800

    add timestamp keyword

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,18 +1,18 @@
-import { SubstrateEvent } from "@subql/types";
-import { Transfer } from "../types";
-import { Balance } from "@polkadot/types/interfaces";
+import { SubstrateEvent } from '@subql/types'
+import { Transfer } from '../types'
+import { Balance } from '@polkadot/types/interfaces'
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  const from = event.event.data[0];
-  const to = event.event.data[1];
-  const amount = event.event.data[2];
+  const from = event.event.data[0]
+  const to = event.event.data[1]
+  const amount = event.event.data[2]
+  const datetime = new Date()
   const transfer = new Transfer(
-	`${event.block.block.header.number.toNumber()}-${event.idx}`
-        );
-       transfer.blockNumber = event.block.block.header.number.toBigInt();
-       transfer.from = from.toString();
-       transfer.to = to.toString();
-       transfer.amount = (amount as Balance).toBigInt();
-       await transfer.save();
+    `${event.block.block.header.number.toNumber()}-${event.idx}`
+  )
+  transfer.blockNumber = event.block.block.header.number.toBigInt()
+  transfer.from = from.toString()
+  transfer.to = to.toString()
+  transfer.amount = (amount as Balance).toBigInt()
+  transfer.timestamp = event.block.timestamp || datetime
+  await transfer.save()
 }
-
-
